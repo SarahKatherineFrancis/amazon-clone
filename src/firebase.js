9ce5dce72f835fac6b4a1e2b6beaa9a7118b8d8b
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-07-29 10:10:42 +0200

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCXB8nECrCP16eOsIztCgtlfCAATprlQJs",
+  authDomain: "clone-37425.firebaseapp.com",
+  projectId: "clone-37425",
+  storageBucket: "clone-37425.appspot.com",
+  messagingSenderId: "903210715322",
+  appId: "1:903210715322:web:0adcddd5b1312fcae0da83",
+  measurementId: "G-LJJVVKDBW1",
+};
