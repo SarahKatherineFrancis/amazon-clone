124bc7f29c963e0f27e1755288ccf81530bc64a3
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-07-30 18:14:13 +0200

    created routing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import "./App.css";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home"
+import Checkout from "./components/checkout/Checkout";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      {/* Header */}
-      <Header />
-      {/* Home */}
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
