dff893da0f062403374df3b6e93cdf7e1ae07c78
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-07-31 20:03:01 +0200

    add CheckoutProduct component for rendering items

--- src/components/checkout/Checkout.jsx
+++ src/components/checkout/Checkout.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import "./checkout.css"
 import AD1 from "../../assets/ad1.jpg"
 import Subtotal from "./Subtotal"
+import CheckoutProduct from './CheckoutProduct'
+import { useStateValue } from '../../StateProvider'
 
 
 const Checkout = () => {
+  const [{basket}, dispatch] = useStateValue()
   return (
     <div className='checkout'>
       <div className="checkout__left">
@@ -14,6 +17,15 @@ const Checkout = () => {
           <h2 className="checkout__title">
             Your Shopping Basket
           </h2>
+
+          {basket.map(item =>
+        <CheckoutProduct
+          id= {item.id}
+          image= {item.image}
+          title= {item.title}
+          price= {item.price}
+          rating= {item.rating}
+        />)}
         </div>
       </div>
 
---
+++ src/components/checkout/CheckoutProduct.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./checkout-product.css"
+
+const CheckoutProduct = ({id, image, title, price, rating}) => {
+  return (
+    <div className='checkoutProduct'>
+      <img src={image} className='checkout-product__image' alt={title} />
+
+      <div className="checkout-product___info">
+        <p className='checkout-product__title'>{title}</p>
+        <p className="checkout-product__price"><small>$</small><strong>{price}
+        </strong></p>
+        <div className="checkout-product__rating">
+          {Array(rating).fill().map((_, i) =>
+          <p>⭐️</p>
+          )}
+        </div>
+        <button>Remove from basket</button>
+      </div>
+    </div>
+  )
+}
+
+export default CheckoutProduct
