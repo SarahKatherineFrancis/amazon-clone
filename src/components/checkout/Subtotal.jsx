8cfb78f2558ab9732bba5ac5b4335d627efa8504
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-07-31 19:37:15 +0200

    display Subtotal with item count and total price

--- src/components/checkout/Subtotal.jsx
+++ src/components/checkout/Subtotal.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import "./subtotal.css"
 // import CurrencyFormat from 'react-currency-format'
+import {useStateValue} from "../../StateProvider"
+import { getBasketTotal } from '../../reducer'
 
 const Subtotal = () => {
+  const [{basket}, dispatch] = useStateValue()
   return (
     <div className='subtotal'>
       <p>
-        Subtotal(0 items):
-        <strong>0</strong>
+        Subtotal ({basket.length} items): $<strong>{getBasketTotal(basket)}
+        </strong>
       </p>
       <small className="subtotal__gift">
         <input type="checkbox" /> This order contains a gift
