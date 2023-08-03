93c1087e49d0623f408e459ceca89523c5dffbe8
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-08-03 20:05:42 +0200

    add payment processing and UI changes

--- src/components/payment/Payment.jsx
+++ src/components/payment/Payment.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./payment.css";
 import { useStateValue } from "../../StateProvider";
 import CheckoutProduct from "../checkout/CheckoutProduct";
 import { Link } from "react-router-dom";
 import { useElements, useStripe, CardElement } from "@stripe/react-stripe-js";
+import { getBasketTotal } from "../../reducer";
 
 const Payment = () => {
   const [{ basket, user }, dispatch] = useStateValue();
 
   const stripe = useStripe();
   const elements = useElements();
+
+  const [succeeded, setSucceded] = useState(false);
+  const [processing, setProcessing] = useState("");
+  const [error, setError] = useState(null);
+  const [disabled, setDisabled] = useState(true);
+
+  const handleSubmit = (e) => {};
+
+  const handleChange = (e) => {
+    setDisabled(e.empty);
+    setError(e.error ? e.error.message : "");
+  };
 
   return (
     <div className="payment">
@@ -48,8 +61,15 @@ const Payment = () => {
             <h3>Payment Method</h3>
           </div>
           <div className="payment__details">
-            <form>
-              <CardElement />
+            <form onSubmit={handleSubmit}>
+              <CardElement onChange={handleChange} />
+
+              <div className="payment__price-container">
+                <h3>Order Total: ${getBasketTotal(basket)}</h3>
+                <button disabled={processing || disabled || succeeded}>
+                  <span>{processing ? <p>Processing</p> : "Buy Now"}</span>
+                </button>
+              </div>
             </form>
           </div>
         </div>
