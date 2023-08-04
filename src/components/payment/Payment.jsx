2156eeed40637875f3ff43966cb8bbdc435a7459
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-08-04 18:30:08 +0200

    handle payment processing and client secret fetch

--- src/components/payment/Payment.jsx
+++ src/components/payment/Payment.jsx
@@ -1,24 +1,50 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./payment.css";
 import { useStateValue } from "../../StateProvider";
 import CheckoutProduct from "../checkout/CheckoutProduct";
 import { Link } from "react-router-dom";
 import { useElements, useStripe, CardElement } from "@stripe/react-stripe-js";
 import { getBasketTotal } from "../../reducer";
+import axios from "axios";
 
 const Payment = () => {
+  // Access the global state (basket and user) and the dispatch function from the data layer
   const [{ basket, user }, dispatch] = useStateValue();
 
+  // Access the Stripe context
   const stripe = useStripe();
   const elements = useElements();
 
+  // States for handling payment processing and error messages
   const [succeeded, setSucceded] = useState(false);
   const [processing, setProcessing] = useState("");
   const [error, setError] = useState(null);
   const [disabled, setDisabled] = useState(true);
+  const [clientSecret, setClientSecret] = useState(true);
 
-  const handleSubmit = (e) => {};
+  // Use effect hook to fetch the client secret for payment
+  useEffect(() => {
+    const getClientSecret = async () => {
+      // Make a POST request to the server to get the client secret
+      const response = await axios({
+        method: "post",
+        url: `/payments/create?total=${getBasketTotal(basket) * 100}`,
+      });
+      setClientSecret(response.data.clientSecret);
+    };
+    getClientSecret();
+  }, [basket]);
 
+  // Handle form submission on payment button click
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setProcessing(true);
+
+    // Perform the payment processing using Stripe
+    // const payload = await stripe;
+  };
+
+  // Handle changes in the CardElement
   const handleChange = (e) => {
     setDisabled(e.empty);
     setError(e.error ? e.error.message : "");
@@ -62,14 +88,19 @@ const Payment = () => {
           </div>
           <div className="payment__details">
             <form onSubmit={handleSubmit}>
+              {/* Stripe CardElement */}
               <CardElement onChange={handleChange} />
 
+              {/* Display the order total and the payment button */}
               <div className="payment__price-container">
                 <h3>Order Total: ${getBasketTotal(basket)}</h3>
                 <button disabled={processing || disabled || succeeded}>
                   <span>{processing ? <p>Processing</p> : "Buy Now"}</span>
                 </button>
               </div>
+
+              {/* Display error message if there's any */}
+              {error && <div>{error}</div>}
             </form>
           </div>
         </div>
